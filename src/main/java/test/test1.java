68768db93ca09c4d4dcd5c0edefea708769639d3
gikim <kjd55399997@gmail.com>
2021-06-01 17:28:00 +0900

    200531_2

---
+++ src/main/java/test/test1.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.nio.charset.StandardCharsets;
+
+public class test1 {
+    public static void main(String[] args) {
+        String str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        String str2 = "김김김김김";
+        System.out.println(str.length());
+        System.out.println(str2.length());
+    }
+}
