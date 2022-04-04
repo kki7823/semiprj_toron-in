5f84a24defa1d20e6d8307d84ad004491e276589
kki7823 <kjd55399997@gmail.com>
2022-04-04 15:22:01 +0900

    코드 정리

--- src/main/java/com/toron/servlet/JoinServlet.java
+++ src/main/java/com/toron/servlet/JoinServlet.java
@@ -20,7 +20,6 @@ public class JoinServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("조인서블릿이다! 두포스트 호출 성공");
         request.setCharacterEncoding("UTF-8");
 
         /**파일 업로드**/
